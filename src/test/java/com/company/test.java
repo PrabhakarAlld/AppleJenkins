b6c3a2cf4a104f0a17cd39c68b3106426faea358
Prabhakar <prabhakar.mishra@gspann.com>
2014-11-07 15:18:18 +0530

    Apple Commit

---
+++ src/test/java/com/company/test.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import org.testng.annotations.Test;
+
+public class test {
+	
+	@Test
+	
+	public void mytest() throws InterruptedException
+	{
+		System.out.println("Hello World");
+		Thread.sleep(1000);
+	
+	}
+
+}
